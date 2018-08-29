dca32576559ffaeb691d8bfdb601b9345e1137ce
Kyle Zimmer <kylezimmer2@gmail.com>
2018-08-29 11:03:00 -0500

    fixed M-lab connection config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<kylez>:<aredpeach333>@ds137862.mlab.com:37862/projot-prod'}
+    module.exports = {mongoURI: 'mongodb://kylez:aredpeach333@ds137862.mlab.com:37862/projot-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/projot-dev'}
 }
