e235176e8376081be9b80b8a0fdd86c20b1fb6bc
Kyle Zimmer <kylezimmer2@gmail.com>
2021-02-21 22:50:52 -0600

    DB fix try 2

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb+srv://kylez:aredpeach333@node-auth-crud.e0v4t.mongodb.net/projot?retryWrites=true&w=majority',
+      'mongodb://kylez:aredpeach333@node-auth-crud-shard-00-00.e0v4t.mongodb.net:27017,node-auth-crud-shard-00-01.e0v4t.mongodb.net:27017,node-auth-crud-shard-00-02.e0v4t.mongodb.net:27017/projot?ssl=true&replicaSet=atlas-na4wn4-shard-0&authSource=admin&retryWrites=true&w=majority',
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/projot-dev' };
